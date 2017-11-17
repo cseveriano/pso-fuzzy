87f9ee6afb887d542658321f134243073e6475fd
Carlos Severiano <carlossjr@gmail.com>
2017-11-17 16:39:56 -0200

    first version

---
+++ PSO.py
@@ -0,0 +1,36 @@
+import Particle
+
+class PSO():
+    def __init__(self,x,y,x0,bounds,num_particles,maxiter):
+
+        err_best_g=-1                   # best error for group
+        pos_best_g=[]                   # best position for group
+
+        # establish the swarm
+        swarm=[]
+        for i in range(0,num_particles):
+            swarm.append(Particle(x0))
+
+        # begin optimization loop
+        i=0
+        while i < maxiter:
+            #print i,err_best_g
+            # cycle through particles in swarm and evaluate fitness
+            for j in range(0,num_particles):
+                swarm[j].evaluate(x,y)
+
+                # determine if current particle is the best (globally)
+                if swarm[j].err_i < err_best_g or err_best_g == -1:
+                    pos_best_g=list(swarm[j].position_i)
+                    err_best_g=float(swarm[j].err_i)
+
+            # cycle through swarm and update velocities and position
+            for j in range(0,num_particles):
+                swarm[j].update_velocity(pos_best_g)
+                swarm[j].update_position(bounds)
+            i+=1
+
+        # print final results
+        print('FINAL:')
+        print(pos_best_g)
+        print(err_best_g)
